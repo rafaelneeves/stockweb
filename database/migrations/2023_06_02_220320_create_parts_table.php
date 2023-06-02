5b0a39a2bcae14366e69d7fdcde60ff311c1ac8d
Rafael Neves <61380782+rafaelneeves@users.noreply.github.com>
2023-06-02 19:07:36 -0300

    Migrations

---
+++ database/migrations/2023_06_02_220320_create_parts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('number_part');
+            $table->string('name');
+            $table->integer('amount');
+            $table->double('price');
+            $table->foreignId('brand_id')->constrained();
+            $table->foreignId('company_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parts');
+    }
+};
